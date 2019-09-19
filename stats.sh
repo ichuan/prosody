93bf5d8276627f0fbacbbe3b0bcf912816ee875e
yc <iyanchuan@gmail.com>
2019-09-19 17:36:47 +0800

    fix: add `c2s_connections` to stats

--- stats.sh
+++ stats.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 make_stats () {
-  total_users=$(echo -e "user:list(\"$DOMAIN\")\nbye\n" | nc -w 5 localhost 5582 | sed '/Showing all/!d' | grep -o --color=never '[0-9]*')
-  online_users=$(echo -e "c2s:show()\nbye\n" | nc -w 5 localhost 5582 | sed "/@$DOMAIN/!d" | awk -F / '{print $1}' | sort | uniq | wc -l)
+  total_users=$(echo -en "user:list(\"$DOMAIN\")\nbye\n" | nc -w 5 localhost 5582 | sed '/Showing all/!d' | grep -o --color=never '[0-9]*')
+  onlines=$(echo -en "c2s:show()\nbye\n" | nc -w 5 localhost 5582 | sed "/@$DOMAIN/!d" | awk -F / '{print $1}' | sort)
+  c2s_connections=$(echo "$onlines" | wc -l)
+  online_users=$(echo "$onlines" | uniq | wc -l)
 
   cat << EOF > /www/stats.json
   {
     "total_users": $total_users,
+    "c2s_connections": $c2s_connections,
     "online_users": $online_users
   }
 EOF
