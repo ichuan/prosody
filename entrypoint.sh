38b3a1e2e8d4cbf18b16bfdb060b0d1853882023
yc <iyanchuan@gmail.com>
2019-09-23 18:08:15 +0800

    fix: ulimit tune, fix "Too many open files" errors
    Thanks https://cypherpunk.is/2016/12/01/increase-prosody-open-file-limit/

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 envs=(ADMIN_JID DOMAIN CONTACT_EMAIL RECAPTCHA_PRIVATE RECAPTCHA_PUBLIC)
 
 if test $# -eq 0; then
@@ -35,6 +33,11 @@ if test $# -eq 0; then
       -e "s#{since}#$SINCE#g" /www/index.html
   # stats
   /stats.sh >/dev/null &
+  # limits
+  sysctl fs.file-max=6553560 2>/dev/null
+  [ -f /etc/systemd/system.conf ] && sed -i "s/^#DefaultLimitNOFILE=.*/DefaultLimitNOFILE=500000/g" /etc/systemd/system.conf
+  echo -e "* soft nofile 500000\n* hard nofile 500000\nroot soft nofile 500000\nroot hard nofile 500000" > /etc/security/limits.conf 2>/dev/null
+  echo "MAXFDS=500000" > /etc/default/prosody
   exec su -c "/usr/bin/prosody" --preserve-environment -s /bin/sh prosody
 else
   exec $@
