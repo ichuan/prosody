c54c397d7ffd15c15e8383c6b0915d07a00b0e77
ichuan <iyanchuan@gmail.com>
2020-03-19 23:11:53 +0800

    fix: reload prosody after ssl renew

--- import_certs.sh
+++ import_certs.sh
@@ -3,3 +3,6 @@
 cp -f /root/.acme.sh/${DOMAIN}/fullchain.cer /etc/prosody/certs/${DOMAIN}.crt
 cp -f /root/.acme.sh/${DOMAIN}/${DOMAIN}.key /etc/prosody/certs/${DOMAIN}.key
 chmod 777 /etc/prosody/certs/${DOMAIN}.{crt,key}
+
+# try reload http after renew
+echo -en "module:reload('http','616.pub')\nbye\n" | nc -w 5 localhost 5582
