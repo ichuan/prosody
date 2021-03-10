0b75b64c6f50a387a234c54dd047c0fd11610ec3
ichuan <iyanchuan@gmail.com>
2021-03-10 16:16:51 +0800

    fix: correctly reload SSL

--- import_certs.sh
+++ import_certs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cp -f /root/.acme.sh/${DOMAIN}/fullchain.cer /etc/prosody/certs/${DOMAIN}.crt
-cp -f /root/.acme.sh/${DOMAIN}/${DOMAIN}.key /etc/prosody/certs/${DOMAIN}.key
-chmod 777 /etc/prosody/certs/${DOMAIN}.{crt,key}
+checksums=`md5sum /root/.acme.sh/${DOMAIN}/${DOMAIN}.key /etc/prosody/certs/${DOMAIN}.key | awk '{print $1}' | uniq | wc -l`
 
-# try reload http after renew
-prosodyctl reload
+if [ "$checksums" = "2" ]; then
+  cp -f /root/.acme.sh/${DOMAIN}/fullchain.cer /etc/prosody/certs/${DOMAIN}.crt
+  cp -f /root/.acme.sh/${DOMAIN}/${DOMAIN}.key /etc/prosody/certs/${DOMAIN}.key
+  chmod 777 /etc/prosody/certs/${DOMAIN}.{crt,key}
+  # try reload http after renew
+  echo -en "module:reload('c2s')\nmodule:reload('s2s')\nmodule:reload('http')\nbye\n" | nc -w 5 localhost 5582
+fi
